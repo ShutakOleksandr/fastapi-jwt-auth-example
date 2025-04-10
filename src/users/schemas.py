06accb0604cf0fbc9a832d6d66d00288a7ee46f8
Oleksandr <132876239+ShutakOleksandr@users.noreply.github.com>
2025-04-11 00:23:28 +0300

    Update schemas.py

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,45 +1,49 @@
 import uuid
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    email: Optional[str] = Field(None)
-    fio: Optional[str] = Field(None)
-    is_active: bool = Field(True)
-    is_verified: bool = Field(False)
-    is_superuser: bool = Field(False)
+    email: Optional[str] = None
+    fio: Optional[str] = None
+    is_active: bool = True
+    is_verified: bool = False
+    is_superuser: bool = False
 
 
-class UserCreate(UserBase):
+class UserCreate(BaseModel):
     email: str
     fio: str
     password: str
 
 
-class UserUpdate(UserBase):
+
+class UserUpdate(BaseModel):
+    email: Optional[str] = None
+    fio: Optional[str] = None
     password: Optional[str] = None
+    is_active: Optional[bool] = None
+    is_verified: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+
 
 
 class User(UserBase):
     id: uuid.UUID
-    email: str
-    fio: str
-    is_active: bool
-    is_verified: bool
-    is_superuser: bool
 
     class Config:
         from_attributes = True
 
 
-class UserCreateDB(UserBase):
-    hashed_password: Optional[str] = None
-
-
-class UserUpdateDB(UserBase):
+# Модель для сохранения пользователя в БД (с хешированным паролем)
+class UserCreateDB(BaseModel):
     hashed_password: str
+
+
+class UserUpdateDB(BaseModel):
+    hashed_password: str
+
 
 
 class RefreshSessionCreate(BaseModel):
@@ -48,8 +52,12 @@ class RefreshSessionCreate(BaseModel):
     user_id: uuid.UUID
 
 
-class RefreshSessionUpdate(RefreshSessionCreate):
-    user_id: Optional[uuid.UUID] = Field(None)
+
+class RefreshSessionUpdate(BaseModel):
+    refresh_token: Optional[uuid.UUID] = None
+    expires_in: Optional[int] = None
+    user_id: Optional[uuid.UUID] = None
+
 
 
 class Token(BaseModel):
